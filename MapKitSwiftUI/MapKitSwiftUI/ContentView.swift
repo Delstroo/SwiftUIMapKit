1a129210cae6d05a4110c1583496d7fb60455ddf
Delstun McCray <87548497+Delstroo@users.noreply.github.com>
2024-01-08 11:54:50 -0800

    adding distances to the list of directions

--- MapKitSwiftUI/MapKitSwiftUI/ContentView.swift
+++ MapKitSwiftUI/MapKitSwiftUI/ContentView.swift
@@ -12,10 +12,10 @@ struct ContentView: View {
     
     @State private var directions: [String] = []
     @State private var showDirections = false
-    
+    @State private var distances: [Double] = []
     var body: some View {
         VStack {
-            MapView(directions: $directions)
+            MapView(directions: $directions, distances: $distances)
             
             Button(action: {
                 showDirections.toggle()
@@ -36,12 +36,34 @@ struct ContentView: View {
                 
                 List {
                     ForEach(0..<self.directions.count, id: \.self) { i in
-                        Text(self.directions[i])
-                            .padding()
+                        HStack {
+                            Text(self.directions[i])
+                            
+                            Spacer()
+                            
+                            Text("\(self.roundDistance(meters: distances[i + 1]))")
+                                .frame(width: 50, alignment: .trailing)
+                        }
+                        .padding()
                     }
                 }
             }
         })
+    }
+    
+    func roundDistance(meters: Double) -> String {
+        let feet = meters * 3.28084 // Convert meters to feet
+        let miles = feet / 5280.0
+        
+        if miles < 0.25 {
+            return "\(Int(feet)) feet"
+        } else if miles >= 1.0 {
+            let roundedMiles = (miles * 10).rounded() / 10 // Round to nearest tenth of a mile
+            return "\(roundedMiles) miles"
+        } else {
+            let roundedMiles = miles.rounded()
+            return "\(roundedMiles) mile"
+        }
     }
 }
 
@@ -54,6 +76,7 @@ struct MapView: UIViewRepresentable {
     typealias UIViewType = MKMapView
     
     @Binding var directions: [String]
+    @Binding var distances: [Double]
     
     func makeCoordinator() -> MapViewCoordinator {
         return MapViewCoordinator()
@@ -63,7 +86,7 @@ struct MapView: UIViewRepresentable {
         mapView.delegate = context.coordinator
         let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 45.8590, longitude: -122.8212), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
         mapView.setRegion(region, animated: true)
-
+        
         // MARK: St. Helens
         let p1 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 45.87527847057365, longitude: -122.81023778547092))
         
@@ -81,6 +104,7 @@ struct MapView: UIViewRepresentable {
         directions.calculate { response, error in
             guard let route = response?.routes.first else { return }
             self.directions = route.steps.map { $0.instructions }.filter { !$0.isEmpty }
+            self.distances = route.steps.map { $0.distance }
             mapView.addAnnotations([p1, p2])
             mapView.addOverlay(route.polyline)
             mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 24, left: 30, bottom: 24, right: 30), animated: true)
@@ -96,7 +120,7 @@ struct MapView: UIViewRepresentable {
         func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
             let renderer = MKPolylineRenderer(overlay: overlay)
             renderer.strokeColor = .orange
-            renderer.lineWidth = 5
+            renderer.lineWidth = 3
             
             return renderer
         }
